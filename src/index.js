4942d58cb59683a1a59fbeb87d3b14576cdba0c8
victorsmith <victor7smith@gmail.com>
2022-01-31 01:56:52 -0700

    minor cleanup

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import chefPic from './images/chef.jpg'
 // Pages
 import renderHeader from './header';
 import renderHome from './home.js';
-import renderMenu from './menu.js';
-import renderContact from './contact.js';
 import renderFooter from './footer';
 
 (function init() {
